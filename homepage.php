63297fee99f48452a641dac8fa3cb10a13573e0f
Bardin Ellis <bardin@headwaydigital.co.za>
2018-09-18 10:21:59 +0200

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ * Template for displaying the homepage without sidebar even if a sidebar widget is published.
+ *
+ * @package understrap
+ */
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="wrapper home-wrapper" id="full-width-page-wrapper">
+<div id="header-area" style="background: url('<?php the_field('header_background_image'); ?>');">
+	<div class="container">
+	<?php the_field('header_text_area'); ?>
+	</div>
+</div>
+<div class="container">
+	<?php the_field('content'); ?>
+</div>
+	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+
+		<div class="row">
+
+			<div class="col-md-12 content-area" id="primary">
+
+				<main class="site-main" id="main" role="main">
+
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php get_template_part( 'loop-templates/content', 'page' ); ?>						
+						
+					<?php endwhile; // end of the loop. ?>
+					
+
+				</main><!-- #main -->
+
+			</div><!-- #primary -->
+
+		</div><!-- .row end -->
+
+	</div><!-- Container end -->
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
